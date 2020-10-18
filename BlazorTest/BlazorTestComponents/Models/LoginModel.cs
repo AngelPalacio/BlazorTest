95e02469d287a598b2fd44ef73033a746960259c
Angel Palacio <angel.palacio.gonzalez@gmail.com>
2020-10-18 20:13:48 +0200

    Add login form validation and model

---
+++ BlazorTest/BlazorTestComponents/Models/LoginModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace BlazorTestComponents.Models
+{
+    public interface ILoginModel
+    {
+        public string Name { get; set; }
+        public string Password { get; set; }
+    }
+
+    public class LoginModel : ILoginModel
+    {
+        [Required]
+        [MinLength(5, ErrorMessage = "The user name must be at least 5 characters long")]
+        public string Name { get; set; }
+
+        [Required]
+        [MinLength(5, ErrorMessage = "The password must be at least 5 characters long")]
+        public string Password { get; set; }
+
+    }
+}
