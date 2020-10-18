94c924e70863bf760dc6ba02141fe6fa89f43007
Angel Palacio <angel.palacio.gonzalez@gmail.com>
2020-10-18 20:19:03 +0200

    Add parameters to login form. Add new login model. Add mock authentication. Add bad password message.

---
+++ BlazorTest/BlazorTest/Models/MyPeculiarLoginModel.cs
@@ -0,0 +1,15 @@
+﻿using BlazorTestComponents.Models;
+using System.ComponentModel.DataAnnotations;
+namespace BlazorTest.Models
+{
+    public class MyPeculiarLoginModel : ILoginModel
+    {
+        [Required]
+        [MinLength(3, ErrorMessage = "The user name must be at least 3 characters long")]
+        public string Name { get; set; }
+
+        [Required]
+        [MinLength(6, ErrorMessage = "The password must be at least 6 characters long")]
+        public string Password { get; set; }
+    }
+}
