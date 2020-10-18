903273e77cf5715bfe8a6265eac234336fa84458
Angel Palacio <angel.palacio.gonzalez@gmail.com>
2020-10-18 21:00:18 +0200

    Add RedirectToLogin component and apply when not authorized

---
+++ BlazorTest/BlazorTest/Shared/RedirectToLogin.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Components;
+
+
+namespace BlazorTest.Shared
+{
+    public class RedirectToLogin : ComponentBase
+    {
+        [Inject]
+        protected NavigationManager NavigationManager { get; set; }
+
+        protected override void OnAfterRender(bool firstRender)
+        {
+            NavigationManager.NavigateTo("/login");
+        }
+    }
+}
